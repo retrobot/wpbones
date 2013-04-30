7cfae80d7dc005ab9d17474bb725aff1aa548bd5
Kamil Kielan <kamilkielan@gmail.com>
2013-04-30 16:04:35 +0100

    Supplementing grunt with additional libs

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,17 +24,49 @@ module.exports = function(grunt) {
 
       }
     },
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'sass',
+  	  cssDir: 'css',
+	  fontsDir: 'fonts',
+	  javascriptDir: 'js',
+	  environment: 'production'
+        }
+      },
+      dev: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'css'
+        }
+      }
+    },
     regarde: {
       txt: {
         files: '**/*.*',
         tasks: ['livereload']
       }
+    },
+    imagemin: {
+      dist: {
+        options: {
+	   optimizationLevel: 3
+	},
+	files: {
+	  'dist/img.png': 'src/img.png',
+	  'dist/img.jpg': 'src/img.jpg'
+	}
+      }
     }
   });
+
   grunt.loadNpmTasks('grunt-regarde');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-livereload');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['livereload-start','connect','regarde']);
+  grunt.registerTask('images', ['imagemin']);
+  grunt.registerTask('default', ['compass','livereload-start','connect','regarde']);
 };
 
